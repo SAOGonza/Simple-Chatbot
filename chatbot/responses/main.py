2edce809144b87ce9f7ad92143690588649695e2
SAOGonza <samuelortega86@yahoo.com>
2023-07-27 15:23:25 -0700

    Created a simple chatbot.

---
+++ chatbot/responses/main.py
@@ -0,0 +1,55 @@
+import re
+import long_responses as long
+
+def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    for word in user_message:
+        if word in recognised_words:
+            message_certainty += 1
+
+    # Calculates the percent of recognised words in a user message
+    percentage = float(message_certainty) / float(len(recognised_words))
+
+    # Check that required words are included as well
+    for word in required_words:
+        if word not in user_message:
+            has_required_words = False
+            break
+
+    # Return accuracy of each sentence so they can be compared and
+    # returned with the best response possible.
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+
+
+def check_all_messages(message):
+    highest_prob_list = {}
+
+    def response(bot_response, list_of_words, single_response=False, required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+    # Response ----------------------------------------
+    response('Hello!', ['hello', 'hi', 'sup', 'hey', 'heyo'], single_response=True)
+    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how']) # We want the bot to understand HOW are you doing in case asked "What are you doing?", so it knows to respond only to how.
+    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
+    response(long.R_EATING, ['what', 'you,' 'eat'], required_words=['you', 'eat'])
+
+    best_match = max(highest_prob_list, key=highest_prob_list.get)
+    #print(highest_prob_list)
+
+    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
+
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower()) # Removes symbols from messages
+    response = check_all_messages(split_message)
+    return response
+
+
+# Testing the response system.
+while True:
+    print('Bot: ' + get_response(input('You: ')))
